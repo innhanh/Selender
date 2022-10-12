8e2c362884ca3b589bbc7f9697f23794ee58efd5
innhanh <qcinnhanh7979@gmail.com>
2022-10-12 10:05:02 +0700

    update Server Cors + Domain

--- server.js
+++ server.js
@@ -1,36 +1,29 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const express = require("express"); // Sử dụng framework express
+const next = require("next"); // Include module next
+const cors = require("cors");
+const dotenv = require("dotenv").config();
 
-const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = 3000
-// when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port })
-const handle = app.getRequestHandler()
+const port = process.env.PORT || 3000 // Port để chạy app Nextjs, cũng là server nodejs
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev })
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-    createServer(async (req, res) => {
-        try {
-            // Be sure to pass `true` as the second argument to `url.parse`.
-            // This tells it to parse the query portion of the URL.
-            const parsedUrl = parse(req.url, true)
-            const { pathname, query } = parsedUrl
+    const server = express();
+    server.use(cors({
+        origin: ["*"]
+    }));
 
-            if (pathname === '/a') {
-                await app.render(req, res, '/a', query)
-            } else if (pathname === '/b') {
-                await app.render(req, res, '/b', query)
-            } else {
-                await handle(req, res, parsedUrl)
-            }
-        } catch (err) {
-            console.error('Error occurred handling', req.url, err)
-            res.statusCode = 500
-            res.end('internal server error')
-        }
-    }).listen(port, (err) => {
+    server.get('/a', (req, res) => {
+        return app.render(req, res, '/a', req.query)
+    });
+
+    server.all('*', (req, res) => {
+        return handle(req, res)
+    })
+
+    server.listen(port, err => {
         if (err) throw err
-        console.log(`> Ready on http://${hostname}:${port}`)
+        console.log(`Server is running on http://${process.env.DOMAIN_NAME}:${port}`);
     })
 })
